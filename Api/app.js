01764b59fac4a8ac1d4467c799435f20c31dab62
Kevin Cruz <devs-dgrv@ip.gob.hn>
2024-05-27 14:15:01 -0600

    add

--- Api/app.js
+++ Api/app.js
@@ -1,10 +1,22 @@
 const express =require('express')
+const movies = require('./movies.json')
 
 const app =express()
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
     res.json({message: 'hola mundo'})
+})
+app.get('/movies', (req, res) => {
+    res.json(movies)
+})
+
+//path to reged convierte en expresiones regulares 
+app.get('/movies/:id',(req, res)=>{
+    const { id } = req.params
+
+
+
 })
 
 const PORT = process.env.PORT ?? 1234
