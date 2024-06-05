175b406289138b43c5473900ddf4748a4c71c22c
Kevin Cruz <devs-dgrv@ip.gob.hn>
2024-06-05 11:13:47 -0600

    add

--- Api/app.js
+++ Api/app.js
@@ -1,22 +1,54 @@
 const express =require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto')
-const { validatePartialMovie } = require('./movies')
+const { validatePartialMovie } = require('./schemas/movies')
+const cors = require('cors')
+
+
 
 
 
 const app =express()
 app.use(express.json())
 
+app.use(cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        'http://localhost:5580',
+        'http://localhost:1234',
+        'https://movies.com',
+        'https://midu.dev'
+      ]
+  
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true)
+      }
+  
+      if (!origin) {
+        return callback(null, true)
+      }
+  
+      return callback(new Error('Not allowed by CORS'))
+    }
+  }))
+
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-    res.json({message: 'hola mundo'})
-})
+// app.get('/', (req, res) => {
+//     res.json({message: 'hola mundo'})
+// })
+// app.get('/movies', (req, res) => {
+//     res.json(movies)
+// })
+
 app.get('/movies', (req, res) => {
-    res.json(movies)
-})
-app.get('/movies', (req, res) => {
+    const origin = req.header('origin' ||  !origin)
+
+    if (ACCEPTED_ORIGINS.includes(origin)){
+        res.header('Access-Control-Allow-Origin',  origin)
+    }
+
+    
     const { genre } = req.query
     if ( genre) {
         const filteredMovies = movies.filter(
@@ -38,6 +70,7 @@ app.get('/movies/:id',(req, res)=>{
 
 app.post('/movies' ,(req, res)=>{
 
+
     const result = validarMovie(req.body)
 
     if (result.error) {
@@ -53,6 +86,25 @@ app.post('/movies' ,(req, res)=>{
 
     movies.push(newMovie)
     res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req,res)=> {
+    const origin = req.header('origin' ||  !origin)
+
+    if (ACCEPTED_ORIGINS.includes(origin)){
+        res.header('Access-Control-Allow-Origin',  origin)
+    }
+
+    const {id} = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id )
+    if (movieIndex === -1){
+        return res.status(404).json({message: 'movie not found'})
+    }
+    movie.splice(movieIndex,1)
+    return res.json({ message : ' movie deleted'})
+
+
+
 })
 
 app.patch ('/movies/:id', (req,res)=> {
